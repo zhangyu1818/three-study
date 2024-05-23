8a688af801449db65d0e376ec6ab7430ac8bdbe0
ZHANGYU <hey@zhangyu.dev>
2024-05-24 00:27:44 +0800

    第11课：MeshStandardMaterial

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,69 @@
+import { Pane } from 'tweakpane'
+
 import * as THREE from 'three'
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
 
 import './style.css'
 
 const canvas = document.querySelector<HTMLCanvasElement>('canvas.webgl')!
 
+const pane = new Pane()
+
 // 创建场景
 const scene = new THREE.Scene()
 
-// 创建Box几何体
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-// 创建材质
-const material = new THREE.MeshBasicMaterial({ color: 0x38bdf8 })
-// 用几何体和材质创建网格
-const mesh = new THREE.Mesh(geometry, material)
+const rgbeLoader = new RGBELoader()
+rgbeLoader.load('/textures/environmentMap/2k.hdr', (environmentMap) => {
+  environmentMap.mapping = THREE.EquirectangularReflectionMapping
 
-// 将网格添加到场景
-scene.add(mesh)
+  scene.environment = environmentMap
+  scene.background = environmentMap
+})
+
+const textureLoader = new THREE.TextureLoader()
+const colorTexture = textureLoader.load('/textures/door/color.jpg')
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load(
+  '/textures/door/ambientOcclusion.jpg',
+)
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
+colorTexture.colorSpace = THREE.DisplayP3ColorSpace
+
+// 创建材质
+const material = new THREE.MeshStandardMaterial({
+  metalness: 1,
+  roughness: 1,
+  side: THREE.DoubleSide,
+  map: colorTexture,
+  aoMap: ambientOcclusionTexture,
+  aoMapIntensity: 1,
+  displacementMap: heightTexture,
+  displacementScale: 0.1,
+  metalnessMap: metalnessTexture,
+  roughnessMap: roughnessTexture,
+  normalMap: normalTexture,
+  transparent: true,
+  alphaMap: alphaTexture,
+})
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 64, 64), material)
+sphere.position.x = -1.5
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1, 100, 100), material)
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 64, 128),
+  material,
+)
+torus.position.x = 1.5
+
+scene.add(sphere, plane, torus)
 
 const sizes = {
   width: window.innerWidth,
@@ -53,7 +99,33 @@ window.addEventListener('resize', () => {
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+// Tweaks
+
+pane.addBinding(material, 'metalness', {
+  min: 0,
+  max: 1,
+  step: 0.01,
+})
+
+pane.addBinding(material, 'roughness', {
+  min: 0,
+  max: 1,
+  step: 0.01,
+})
+
+const clock = new THREE.Clock()
+
 const tick = () => {
+  const elapsedTime = clock.getElapsedTime() / 5
+
+  sphere.rotation.y = elapsedTime
+  plane.rotation.y = elapsedTime
+  torus.rotation.y = elapsedTime
+
+  sphere.rotation.x = -elapsedTime
+  plane.rotation.x = -elapsedTime
+  torus.rotation.x = -elapsedTime
+
   // 渲染
   orbitControls.update()
 
